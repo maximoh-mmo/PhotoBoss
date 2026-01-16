c269669a7988ba4057717f3cf0d08f853d1c55b3
Max Heinze <maxheinze@googlemail.com>
2026-01-16 22:37:11 +0100

    Added fingerprint, for file identification

--- photoboss/inc/photoboss/DataTypes.h
+++ photoboss/inc/photoboss/DataTypes.h
@@ -2,25 +2,49 @@
 #include <qdatetime.h>
 
 namespace photoboss {
-    struct ImageFileMetaData
+    struct Fingerprint
     {
+    public:
         QString path;
-        quint64 size;
-        QDateTime lastModified;
-        QString suffix;
+        // File format (e.g., "JPEG", "PNG", etc.)
+        QString format;
+        // Fast Change Identifier (FCI) for quick change detection
+        quint64 size = 0;
+        quint64 modifiedTime = 0;
+        // MD5 hash of the file contents
+        QString md5;
+        bool md5Computed = false;
     };
-    
+
     struct DiskReadResult {
-        ImageFileMetaData meta;
+        Fingerprint fingerprint;
         QByteArray imageBytes;
     };
 
     struct HashedImageResult {
-        ImageFileMetaData meta;
+        Fingerprint fingerprint;
         std::map<QString, QString> hashes;  // SHA256, pHash, etc.
     };
+    
     struct Group {
         QString name;
         QString path;
     };
+
+    struct HashConfig {
+        QString name;
+        bool enabled;
+    };
+
+    enum class PipelineState {
+        Stopped,
+        Starting,
+        Idle,
+        Scanning,
+        Stopping
+    };
+
+    using LoadedImagePtr = std::shared_ptr<DiskReadResult>;
+    using HashResultPtr = std::shared_ptr<HashedImageResult>;
+    using FileMetaListPtr = std::shared_ptr<std::vector<Fingerprint>>;
 }
